a52fb259482673c0d4cdaf2b920d73e213d948ae
Uday-2611 <udayagarwal234@gmail.com>
2025-04-17 04:00:25 +0530

    fixed cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,17 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-    cloud_name: 'dxqjyqz8p',
-    api_key: '583153789193697',
-    api_secret: 'Ux_Ry6xKYB_3RzR9tGD9GQqVlYw'
-});
+const connectCloudinary = () => {
+    try {
+        cloudinary.config({
+            cloud_name: 'dxqjyqz8p',
+            api_key: '583153789193697',
+            api_secret: 'Ux_Ry6xKYB_3RzR9tGD9GQqVlYw'
+        });
+        console.log('Cloudinary Connected Successfully');
+    } catch (error) {
+        console.error('Cloudinary Connection Failed:', error);
+        process.exit(1);
+    }
+};
 
-export default cloudinary;
+export { cloudinary, connectCloudinary };
