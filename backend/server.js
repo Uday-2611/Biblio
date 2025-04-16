6ac7ec12707cd22cb01eef3fc626d008aaa1d318
Uday-2611 <udayagarwal234@gmail.com>
2025-04-17 03:22:01 +0530

    localhost

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,12 @@ connectCloudinary();
 // ------ Middlewares ------
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'token']
+}));
 app.use('/uploads', express.static('uploads'));
 
 // ------ API Endpoints ------
